b91df5bd9f29e9faab7d47b24f0536d80b50d1db
AlejandroPinto99 <00156618@uca.edu.sv>
2022-10-01 00:00:45 -0500

    Initial commit

--- src/components/dashboard/dashboard-sidebar.js
+++ src/components/dashboard/dashboard-sidebar.js
@@ -32,7 +32,6 @@ import { DashboardSidebarSection } from './dashboard-sidebar-section';
 import { OrganizationPopover } from './organization-popover';
 
 // Icons
-// import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
 import GavelIcon from '@mui/icons-material/Gavel';
 
 const getSections = (t) => [
